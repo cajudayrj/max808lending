e9b962f6c31102fb5b13b1a6c76ce766b588d937
Rey Jude Cajuday <rey.jude.cajuday@codeandtheory.com>
2019-12-19 16:40:13 +0800

    devug login

--- server/routes/middleware/middleware.js
+++ server/routes/middleware/middleware.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken');
 
 const userMiddleware = async (req, res, next) => {
   const token = req.header('auth_token');
-  console.log(req.headers.auth_token);
-  if (!token) return res.json({ success: false, error: { message: "Access denied. Invalid request." } });
+  next();
+  // if (!token) return res.json({ success: false, error: { message: "Access denied. Invalid request." } });
 
-  try {
-    const verified = jwt.verify(token, process.env.APP_TOKEN);
-    req.user = verified;
-    next();
-  } catch (err) {
-    return res.json({ success: false, error: { message: "You don't have permission to make this request." } });
-  }
+  // try {
+  //   const verified = jwt.verify(token, process.env.APP_TOKEN);
+  //   req.user = verified;
+  //   next();
+  // } catch (err) {
+  //   return res.json({ success: false, error: { message: "You don't have permission to make this request." } });
+  // }
 }
 
 module.exports = userMiddleware;
