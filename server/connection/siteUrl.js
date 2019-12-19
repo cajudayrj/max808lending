075221950228eb49b060c58a126a725a7f1deb77
Rey Jude Cajuday <rey.jude.cajuday@codeandtheory.com>
2019-12-19 16:38:42 +0800

    debug login

--- server/connection/siteUrl.js
+++ server/connection/siteUrl.js
@@ -1,2 +1,2 @@
-const siteUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'https://max808lending.com' : 'http://10.64.0.198:3000';
+const siteUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'http://max808lending.com' : 'http://10.64.0.198:3000';
 module.exports = siteUrl;
