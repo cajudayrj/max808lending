a6dd5183b707bdabb6f5403ab5ec6ad6cf54596c
Rey Jude Cajuday <rey.jude.cajuday@codeandtheory.com>
2019-12-19 16:51:04 +0800

    debug login

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "auth_token");
   next();
 })
 
