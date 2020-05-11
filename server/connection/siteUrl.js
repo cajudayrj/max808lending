d52a28e2a89706ad9f0d2c5c9e9f79006bd69d19
Rey Jude Cajuday <rey.jude.cajuday@codeandtheory.com>
2020-05-11 13:17:02 +0800

    Domain reinstallation

--- server/connection/siteUrl.js
+++ server/connection/siteUrl.js
@@ -1,3 +1,2 @@
-// const siteUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'https://max808lending.com' : 'http://192.168.100.15:3000';
-const siteUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'http://157.245.58.162' : 'http://192.168.100.15:3000';
+const siteUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'https://max808lending.com' : 'http://192.168.100.15:3000';
 module.exports = siteUrl;
