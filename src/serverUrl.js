49bfa78273d5551b315e01a5550c02abf398ef2e
Rey Jude Cajuday <rey.jude.cajuday@codeandtheory.com>
2020-05-11 10:53:52 +0800

    tempo site fix

--- src/serverUrl.js
+++ src/serverUrl.js
@@ -1,2 +1,3 @@
-const serverUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'https://max808lending.com/api' : 'http://192.168.100.15:1227';
+// const serverUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'https://max808lending.com/api' : 'http://192.168.100.15:1227';
+const serverUrl = process.env.REACT_APP_DEV_ENV === 'production' ? 'http://157.245.58.162/api' : 'http://192.168.100.15:1227';
 module.exports = serverUrl;
